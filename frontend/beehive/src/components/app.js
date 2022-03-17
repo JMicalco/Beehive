7726b2f113a5506944276c3e3b0068d88f67ca82
José Adolfo <jmicalco@gmail.com>
2022-03-16 20:18:36 -0600

    Simple form and css modification

--- frontend/beehive/src/components/app.js
+++ frontend/beehive/src/components/app.js
@@ -4,14 +4,14 @@ import { Router } from 'preact-router';
 import Header from './header';
 
 // Code-splitting is automated for `routes` directory
-import Home from '../routes/home';
+import TodoForm from '../routes/home';
 import Profile from '../routes/profile';
 
 const App = () => (
 	<div id="app">
 		<Header />
 		<Router>
-			<Home path="/" />
+			<TodoForm path="/" />
 			<Profile path="/profile/" user="me" />
 			<Profile path="/profile/:user" />
 		</Router>
--- frontend/beehive/src/routes/home/index.js
+++ frontend/beehive/src/routes/home/index.js
@@ -1,11 +1,29 @@
 import { h } from 'preact';
 import style from './style.css';
 
+
 const Home = () => (
 	<div class={style.home}>
-		<h1>Home</h1>
-		<p>This is the Home component.</p>
+		<div class={style.img}>
+			<img src="../assets/icons/beehive-logo1.png" alt="Logo"/>
+		</div>
+		<form action="">
+			<h1>¿Cuantas Personas hay con usted?</h1>
+			<input type="text" />
+			<h1>¿CuantOS Menores de edad?</h1>
+			<input type="text" />
+			<h1>¿Cuantos heridos?</h1>
+			<input type="text" />
+			<h1>¿Necesitas agua?</h1>
+			<input type="text" />
+			<h1>¿Necesitas comida?</h1>
+			<input type="text" />
+			<h1>¿Necesitas medicamentos?</h1>
+			<input type="text" />
+		</form>
 	</div>
 );
+
+
 
 export default Home;
